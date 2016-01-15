0f2bc4786a6fd45f8055edda031e288ac59342e8
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2016-01-15 15:36:22 -0800

    Webpack dev vs production.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const webpack = require('webpack');
+const env = process.env.NODE_ENV
+
+
 module.exports = {
-    entry: [
+    entry: env=='development'?[
         'webpack-dev-server/client?http://0.0.0.0:8090', // WebpackDevServer host and port
         'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
         './app/js/app.js' // Your appʼs entry point
-    ],
+    ]:'./app/js/app.js',
     output: {
         filename: 'bundle.js',
         publicPath: '/app/js/',
